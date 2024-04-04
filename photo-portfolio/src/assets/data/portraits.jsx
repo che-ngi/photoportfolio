a59cf8602ae6bf9f13f7218b6539b0aa439ff3bf
Chengi <aaroncheng@umass.edu>
2024-04-04 03:23:28 -0400

    added maria arcade portraits

--- photo-portfolio/src/assets/data/portraits.jsx
+++ photo-portfolio/src/assets/data/portraits.jsx
@@ -1,7 +1,9 @@
 const PORTRAITS = [
     "https://i.imgur.com/VzlB3Rl.jpeg", "https://i.imgur.com/CUblXvK.jpeg", "https://i.imgur.com/4tiGcAq.jpeg", "https://i.imgur.com/VI2b5DO.jpeg",
      "https://i.imgur.com/IIqyZH1.jpeg", "https://i.imgur.com/sIeiNNH.jpeg", "https://i.imgur.com/wB7A5T8.jpeg", "https://i.imgur.com/MRlDyjZ.jpeg",
-      "https://i.imgur.com/hXJSwgo.jpeg", "https://i.imgur.com/fcCd5yQ.jpeg", "https://i.imgur.com/ngsXwh3.jpeg", "https://i.imgur.com/QHqamJg.jpeg", "https://i.imgur.com/L2AXyjs.jpeg"
+      "https://i.imgur.com/hXJSwgo.jpeg", "https://i.imgur.com/fcCd5yQ.jpeg", "https://i.imgur.com/ngsXwh3.jpeg", "https://i.imgur.com/QHqamJg.jpeg", 
+      "https://i.imgur.com/L2AXyjs.jpeg", "https://i.imgur.com/rW3HCxH.jpeg", "https://i.imgur.com/cN8sc19.jpeg",
+      "https://i.imgur.com/X5lTYsI.jpeg", "https://i.imgur.com/WBKGJPY.jpeg", "https://i.imgur.com/P5phCNM.jpeg"
 ]
 
 
